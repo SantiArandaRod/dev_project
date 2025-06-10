ad065cb03adf8edf72483bf7693c56b21aea4d53
dsar <dsaranda89@ucatolica.edu.co>
2025-06-09 20:20:50 -0500

    use of enum for sort modelsd

---
+++ utils/terms.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Kind(Enum):
+    Old = "Old"
+    New = "New"
+    TwoPlayer = "TwoPlayer"
+
+class genre(Enum):
+    Type = "Type"
+    Company = "Company"
+    Units_sold = "Units_sold"
